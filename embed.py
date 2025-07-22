e807f751d5ce71ba05d22f32ff433559c385a58a
Nagi <naginagiyev03@gmail.com>
2025-07-22 18:36:45 +0400

    cleaning code

---
+++ embed.py
@@ -0,0 +1,18 @@
+import os
+import pickle
+import openl3
+from tqdm import tqdm
+import soundfile as sf
+
+embeddings_dict = {}
+folder_path = "./songs"
+
+for basename in tqdm(os.listdir(folder_path)[300:], desc="Getting Embeddings"):
+    file_path = os.path.join(folder_path, basename)
+    audio, sr = sf.read(file_path)
+    embeddings, ts = openl3.get_audio_embedding(audio, sr, input_repr="mel256", content_type="music", embedding_size=512)
+    embedding_vector = embeddings.mean(axis=0)
+    embeddings_dict[basename] = embedding_vector
+
+with open("embedings.pkl", "wb") as f:
+    pickle.dump(embeddings_dict, f)
